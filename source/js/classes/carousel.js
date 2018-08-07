56f0af4b05fd1b32555799f30de0c5ef096b6126
Marco <marco.condoric@gmail.com>
2018-08-07 14:37:14 -0500

    ordena js eb funciones y valida el la existencia de las clases

--- source/js/classes/carousel.js
+++ source/js/classes/carousel.js
@@ -4,24 +4,50 @@ class Carousel{
 
 
   constructor(className, contentElements, classIndicator) {
-    this.name = className;
+    this.className = className;
     this.contentElements = contentElements;
-    this.indicator = classIndicator;
+    this.classIndicator = classIndicator;
   }
 
+  initial(){
+    var CarouselM = document.querySelector('.' + this.className);
+    var mobileCarousel = CarouselM.querySelector('.' + this.contentElements);
+    var mobileCarouselItem = mobileCarousel.querySelectorAll('li');
 
-  createIndicator(){
-    var classParent = document.querySelector('.' + this.name);
-    var contentElements = document.querySelector('.' + this.contentElements);
+    this.addIndicators(mobileCarouselItem);
+
+    var dotsCarousel = CarouselM.querySelectorAll('.' + this.classIndicator + ' li');
+
+    for (let [index, dotItem] of dotsCarousel.entries()) {
+      dotItem.onclick = function(){
+        this.activeElement(dotsCarousel, index);
+        var move = CarouselM.offsetWidth;
+
+        mobileCarousel.scroll({
+          left: move * index,
+          behavior: "smooth"
+        });
+      }
+    }
+  }
+
+  addIndicators(mobileCarouselItem){
     var aBlock = document.createElement('ul'); // crea el elemento
-    aBlock.classList.add(this.indicator); // añade clase
+    aBlock.classList.add(this.classIndicator); // añade clase
     classParent.appendChild(aBlock); //agrega el indicador en el carousel
-    var element = document.querySelector('.' + this.indicator);
+    var element = document.querySelector('.' + this.classIndicator);
 
-     mobileCarouselItem.forEach(() =>{
-        var aBlock = document.createElement('li');
-        element.appendChild(aBlock);
-     })
+    mobileCarouselItem.forEach(() =>{
+      var aBlock = document.createElement('li');
+      element.appendChild(aBlock);
+    })
   }
-
+  activeElement(elements, indexActive){
+    //añade clase active al elemento
+    //quita a los demas que no esten en el index
+    elements.forEach((element) => {
+      element.classList.remove('active');
+    })
+    elements[indexActive].classList.add('active');
+  }
 }
--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,70 @@
-M.AutoInit();
+'use strict';
+
+(() => {
+  // materialize initial
+  M.AutoInit();
+
+  //scroll animation
+  if (document.body.contains(document.querySelector(".c-navbar--fixed"))) {
+    document.addEventListener('scroll', (e) => {
+      var navbar = document.querySelector(".c-navbar--fixed");
+      if (window.scrollY >=  100) {
+        navbar.classList.add("c-navbar--scrolling");
+      }else{
+        navbar.classList.remove("c-navbar--scrolling");
+      }
+    })
+  }
+
+  if (window.matchMedia("(max-width: 992px)").matches) {
+
+    if (document.body.contains(document.querySelector(".c-carousel-mobile"))){
+      /*carousel*/
+      var CarouselM = document.querySelector('.c-carousel-mobile');
+      var mobileCarousel = CarouselM.querySelector('.c-carousel-mobile__elements');
+      var mobileCarouselItem = mobileCarousel.querySelectorAll('li');
+
+      //mobileCarousel.scrollTo( 100, 0 );
+
+      //creacion de indicadores
+        addIndicators(CarouselM, mobileCarouselItem , 'c-indicators');
+        var dotsCarousel = CarouselM.querySelectorAll('.c-indicators li');
+
+      // responde al click y mueve los elementos con efecto smooth
+        for (let [index, dotItem] of dotsCarousel.entries()) {
+          dotItem.onclick = function(){
+            activeElement(dotsCarousel, index);
+            var move = CarouselM.offsetWidth;
+
+            mobileCarousel.scroll({
+              left: move * index,
+              behavior: "smooth"
+            });
+          }
+        }
+
+      /*end carousel*/
+    }
+
+  }
+  if (document.body.contains(document.querySelector(".c-animation-text-fade"))) {
+    var animationTextFade = document.querySelector(".c-animation-text-fade");
+    var animationTextFadeItem = animationTextFade.querySelectorAll('li');
+
+    animationTextFadeItem[0].classList.add("active");
+
+    animationTextFadeItem.forEach((element) => {
+      var aBlock = document.createElement('br');
+      element.appendChild(aBlock);
+    })
+    createTextAnimation(animationTextFade, 4000);
+  }
 
 
-function activeElement(elements, indexActive){
+/* -----------------------function utilities----------------------*/
+
+  // agrega clase active al dar click en botones
+  function activeElement(elements, indexActive){
     //añade clase active al elemento
     //quita a los demas que no esten en el index
     elements.forEach((element) => {
@@ -9,88 +72,36 @@ function activeElement(elements, indexActive){
     })
     elements[indexActive].classList.add('active');
   }
-
-//scroll animation
-document.addEventListener('scroll', (e) => {
-  var navbar = document.querySelector(".c-navbar--fixed");
-  if (window.scrollY >=  100) {
-    navbar.classList.add("c-navbar--scrolling");
-  }else{
-    navbar.classList.remove("c-navbar--scrolling");
-  }
-})
-
-
-if (window.matchMedia("(max-width: 992px)").matches) {
-
-  var CarouselM = document.querySelector('.c-carousel-mobile');
-  var mobileCarousel = CarouselM.querySelector('.c-carousel-mobile__elements');
-  var mobileCarouselItem = mobileCarousel.querySelectorAll('li');
-
-  //mobileCarousel.scrollTo( 100, 0 );
-
-  //creacion de indicadores
-    addIndicators(CarouselM, 'c-indicators');
-    var dotsCarousel = CarouselM.querySelectorAll('.c-indicators li');
-
-  // responde al click y mueve los elementos con efecto smooth
-    for (let [index, dotItem] of dotsCarousel.entries()) {
-      dotItem.onclick = function(){
-        activeElement(dotsCarousel, index);
-        var move = CarouselM.offsetWidth;
-
-        mobileCarousel.scroll({
-          left: move * index,
-          behavior: "smooth"
-        });
-      }
-    }
-
-  function addIndicators(classParent, classIndicator){
+  //agrega indicadores en carusel
+  function addIndicators(classParent,classElements, classIndicator){
     var aBlock = document.createElement('ul'); // crea el elemento
     aBlock.classList.add(classIndicator); // añade clase
     classParent.appendChild(aBlock); //agrega el indicador en el carousel
     var element = document.querySelector('.' + classIndicator);
 
-     mobileCarouselItem.forEach(() =>{
-        var aBlock = document.createElement('li');
-        element.appendChild(aBlock);
-     })
+    classElements.forEach(() =>{
+      var aBlock = document.createElement('li');
+      element.appendChild(aBlock);
+    })
   }
+  // crea el efecto de animacion de texto
+  function createTextAnimation(textFade, time){
+      var animationTextFadeItem = textFade.querySelectorAll('li');
+      var loop = 1;
+      setInterval((() => {
+        //desaparece el contenedor
+        textFade.classList.toggle("u-fade-out");
+        setTimeout((() => {
+          //cambia el valor y aparece el contenedor
+          activeElement(animationTextFadeItem, loop);
+          textFade.classList.toggle("u-fade-out");
+        }),1000)
+        //da al siguiente valor
+        loop++;
 
-
-}
-
-var animationTextFade = document.querySelector(".c-animation-text-fade");
-
-var animationTextFadeItem = animationTextFade.querySelectorAll('li');
-
-var loopAnimation = 1;
-
-animationTextFadeItem[0].classList.add("active");
-
-animationTextFadeItem.forEach((element) => {
-  var aBlock = document.createElement('br');
-  element.appendChild(aBlock);
-})
-
-var animationFade = setInterval((() => {
-  //desaparece el contenedor
-  animationTextFade.classList.toggle("u-fade-out");
-  setTimeout((() => {
-    //cambia el valor y aparece el contenedor
-    activeElement(animationTextFadeItem, loopAnimation);
-    animationTextFade.classList.toggle("u-fade-out");
-  }),1000)
-  //da al siguiente valor
-  loopAnimation++;
-
-  if (loopAnimation >= animationTextFadeItem.length) {
-    loopAnimation = 0;
+        if (loop >= animationTextFadeItem.length) {
+          loop = 0;
+        }
+      }), time);
   }
-}), 4000);
-
-
-
-
-//let a = () => console.log("Hello World");
+})()
